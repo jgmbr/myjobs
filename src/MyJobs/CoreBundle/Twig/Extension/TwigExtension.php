4c60cd947ed975633bb63a592099c9cbd9c01a3e
JGambier <justine.gambier.1@gmail.com>
2016-12-29 16:08:35 +0100

    Check file exist in twig with creation new twig extension

---
+++ src/MyJobs/CoreBundle/Twig/Extension/TwigExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MyJobs\CoreBundle\Twig\Extension;
+
+class TwigExtension extends \Twig_Extension
+{
+
+    /**
+     * Return the functions registered as twig extensions
+     *
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            'file_exists' => new \Twig_Function_Function('file_exists'),
+        );
+    }
+
+    public function getName()
+    {
+        return 'twig_extension';
+    }
+}
